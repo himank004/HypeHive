0bbbb7bc8cd7f82077a80be221d317203c036dc2
himank004 <106752726+himank004@users.noreply.github.com>
2025-03-23 20:39:32 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,17 +3,12 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:5000",
-      },
-    },
-  },
-  build: {
-    rollupOptions: {
-      external: ["@mui/material"],
-    },
-  },
+	plugins: [react()],
+	server: {
+		proxy: {
+			"/api": {
+				target: "http://localhost:5000",
+			},
+		},
+	},
 });
